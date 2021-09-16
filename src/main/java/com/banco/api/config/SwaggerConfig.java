94e532ff70bed23f1ee1a5b9843b2943586a3120
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2021-09-16 16:14:15 -0300

    Melhorias no Swagger

--- src/main/java/com/banco/api/config/SwaggerConfig.java
+++ src/main/java/com/banco/api/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.banco.api.config;
 
+import com.banco.api.model.Cliente;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
@@ -11,6 +15,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -23,9 +28,19 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.banco.api"))
-                .paths(regex("/api.*"))
+                .paths(PathSelectors.ant("/**"))
                 .build()
-                .apiInfo(metaInfo());
+                .ignoredParameterTypes(Cliente.class)
+                .apiInfo(metaInfo())
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        .name("Authorization")
+                                        .description("Header para Token JWT")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()));
     }
 
     private ApiInfo metaInfo() {
@@ -33,7 +48,7 @@ public class SwaggerConfig {
         ApiInfo apiInfo = new ApiInfo(
                 "Banco API Rest",
                 "API Rest de um sistema de gerenciamento de contas.",
-                "1.1",
+                "1.5",
                 "Terms of Service",
                 new Contact("Guilherme Mendes da Cunha", "https://www.linkedin.com/in/guilherme-mendes-cunha/",
                         "guilherme_mendes2018@outlook.com"),
